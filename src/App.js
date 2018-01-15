7ebeb044974883b5794e07f1a51d6e5cb1a98cc1
Brandon Allred <b32alls@gmail.com>
2018-01-15 16:16:49 -0700

    React mini project day 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 
-// Components
 import EditToggle from './components/EditToggle';
 import ColorChanger from './components/ColorChanger';
 import SizeChanger from './components/SizeChanger';
@@ -8,27 +7,54 @@ import FamilyChanger from './components/FamilyChanger';
 import TextContainer from './components/TextContainer';
 
 class App extends Component {
-  // constructor
-
-  // updateColor
-
-  // updateSize
-
-  // updateFamily
-
-  // updateEditStatus
+  constructor () {
+    super()
+      this.state = { 
+        fontColor: 'black',
+        fontSize: 12,
+        fontFamily: 'monospace',
+        allowEdit: "true"
+      }
+      this.updateColor = this.updateColor.bind( this );
+      this.updateSize = this.updateSize.bind( this );
+      this.updateFamily = this.updateFamily.bind( this );
+      this.updateEditStatus = this.updateEditStatus.bind( this )
+  }
+  updateColor (val) {
+    this.setState ({ fontColor: val })
+  }
+  updateSize (val) {
+    this.setState ({ fontSize: val })
+    }
+  updateFamily (val) {
+      this.setState ({ fontFamily: val })
+    }
+  updateEditStatus (val) {
+    this.setState ({ allowEdit: val })
+  }
 
   render() {
     return (
       <div>
         <div className="headerBar">
-          { /* Render EditToggle */ }
-          { /* Render ColorChanger */ }
-          { /* Render SizeChanger */ }
-          { /* Render FamilyChanger */ }
+          <EditToggle update={this.updateEditStatus}></EditToggle>
+          <ColorChanger update={this.updateColor} 
+          allowEdit={this.state.allowEdit}></ColorChanger>
+          <SizeChanger update={this.updateSize} 
+                      allowEdit={this.state.allowEdit}>
+          </SizeChanger>
+
+          <FamilyChanger update={this.updateFamily}
+                          allowEdit={this.state.allowEdit}>
+
+          </FamilyChanger>
         </div>
         <div className="textArea">
-          { /* Render TextContainer */ }
+          <TextContainer fontColor={this.state.fontColor}
+                          fontSize={this.state.fontSize}
+                          fontFamily={this.state.fontFamily}>
+
+          </TextContainer>
         </div>
       </div>
     )
